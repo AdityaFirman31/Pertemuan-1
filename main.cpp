73402ffc348b9d9c39d446d701147ca84efc9134
AdityaFirman31 <112887411+AdityaFirman31@users.noreply.github.com>
2022-09-08 22:20:57 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama         : Aditya Firman Gani" << endl;
+    cout << "Nim          : A11.2022.14134" << endl;
+    cout << "Alamat       : Kalipucang Kulon RT.04 RW.03" << endl;
+    cout << "Asal Sekolah : SMK Muhammadiyah 3 Mayong" << endl;
+    return 0;
+}
