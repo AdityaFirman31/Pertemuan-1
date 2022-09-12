1f1f371f1f2b24eb5b1e3d82c0b3f00f44097cf3
AdityaFirman31 <112887411+AdityaFirman31@users.noreply.github.com>
2022-09-12 20:06:35 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    /*
+    printf ("Aditya");
+    int = integer
+    float
+    char
+    string
+    */
+
+    int u = 18;
+    float t = 175.5;
+    char b = 'M';
+    string ub = "XL";
+    cout << "Umur saya: " << u;
+    cout << endl;
+    cout << "Tinggi saya: " << t;
+    cout << "\nUkuran baju: " << ub;
+    return 0;
+}
